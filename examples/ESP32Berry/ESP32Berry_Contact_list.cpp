457127ae8a01c9b3789221f49d3235a551fbf78d
renemichelnunes <rene.michel@gmail.com>
2023-09-05 20:17:04 -0300

    Add files via upload
    dfgdfgdfgdfgdfg

--- examples/ESP32Berry/ESP32Berry_Contact_list.cpp
+++ examples/ESP32Berry/ESP32Berry_Contact_list.cpp
@@ -12,21 +12,58 @@ const char* title) : AppBase(display, system, network, title){
 /*for testing only==================================================*/
 static uint32_t btn_cnt = 1;
 
-static void float_btn_event_cb(lv_event_t * e)
+static void add(lv_event_t* e){
+  lv_obj_t* addBtn = lv_event_get_target(e);
+  lv_obj_t* window = lv_obj_get_parent(addBtn);
+  lv_obj_t * list = (lv_obj_t*)lv_event_get_user_data(e);
+
+  lv_obj_t* btn = lv_list_add_btn(list, LV_SYMBOL_CALL, "Contact");
+
+  lv_obj_del(window);
+}
+
+static void add_btn_event_cb(lv_event_t * e)
 {
     lv_event_code_t code = lv_event_get_code(e);
     lv_obj_t * float_btn = lv_event_get_target(e);
 
     if(code == LV_EVENT_CLICKED) {
+      lv_obj_t* window = lv_obj_create(lv_scr_act());
+      lv_obj_set_size(window, 200, 200);
+      lv_obj_align(window, LV_ALIGN_CENTER, 0 , 0);
+      
+      lv_obj_t* txtName = lv_textarea_create(window);
+      lv_obj_set_size(txtName, 160, 40);
+      lv_textarea_set_placeholder_text(txtName, "Name");
+      lv_obj_align(txtName, LV_ALIGN_TOP_MID, 0, 0);
+
+      lv_obj_t* txtLoraAddr = lv_textarea_create(window);
+      lv_obj_set_size(txtLoraAddr, 160, 40);
+      lv_textarea_set_placeholder_text(txtLoraAddr, "LoRa Addr");
+      lv_obj_align(txtLoraAddr, LV_ALIGN_TOP_MID, 0, 40);
+
+      lv_obj_t* btnAdd = lv_btn_create(window);
+      lv_obj_set_size(btnAdd, 40, 40);
+      lv_obj_align(btnAdd, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+
+      lv_obj_t* lblBtnAdd = lv_label_create(btnAdd);
+      lv_label_set_text(lblBtnAdd, "OK");
+      lv_obj_align(lblBtnAdd, LV_ALIGN_CENTER, 0, 0);
+      
+      lv_obj_t * list = (lv_obj_t*)lv_event_get_user_data(e);
+      lv_obj_add_event_cb(btnAdd, add, LV_EVENT_CLICKED, list);
+
+      /*
         lv_obj_t * list = (lv_obj_t*)lv_event_get_user_data(e);
         char buf[32];
-        lv_snprintf(buf, sizeof(buf), "Contact %d", (int)btn_cnt);
-        lv_obj_t * list_btn = lv_list_add_btn(list, LV_SYMBOL_CALL, buf);
+        //lv_snprintf(buf, sizeof(buf), "Contact %d", (int)btn_cnt);
+        //lv_obj_t * list_btn = lv_list_add_btn(list, LV_SYMBOL_CALL, buf);
         btn_cnt++;
 
         lv_obj_move_foreground(float_btn);
 
         lv_obj_scroll_to_view(list_btn, LV_ANIM_ON);
+      */
     }
 }
 /*for testing only==================================================*/
@@ -53,7 +90,7 @@ void AppContactList::draw_ui(){
   lv_obj_set_size(addBtn, 50, 50);
   lv_obj_add_flag(addBtn, LV_OBJ_FLAG_FLOATING);
   lv_obj_align(addBtn, LV_ALIGN_BOTTOM_RIGHT, 0, -lv_obj_get_style_pad_right(list, LV_PART_MAIN));
-  lv_obj_add_event_cb(addBtn, float_btn_event_cb, LV_EVENT_ALL, list);
+  lv_obj_add_event_cb(addBtn, add_btn_event_cb, LV_EVENT_ALL, list);
   lv_obj_set_style_radius(addBtn, LV_RADIUS_CIRCLE, 0);
   lv_obj_set_style_bg_img_src(addBtn, LV_SYMBOL_PLUS, 0);
   lv_obj_set_style_text_font(addBtn, lv_theme_get_font_large(addBtn), 0);
