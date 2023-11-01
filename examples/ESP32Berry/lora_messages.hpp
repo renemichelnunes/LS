058081e561df0c7ad7c55ece8ada7a1e95e6b8c1
renemichelnunes <rene.michel@gmail.com>
2023-11-01 13:09:36 -0300

    asdasdsd

---
+++ examples/ESP32Berry/lora_messages.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <Arduino.h>
+
+struct lora_packet{
+    char id[7];
+    char msg[200];
+};
+
+struct lora_contact_messages{
+    char id[7];
+    std::vector <lora_packet> messages;
+};
+
+class lora_incomming_messages{
+  private:
+    std::vector <lora_contact_messages> messages;
+  public:
+    lora_incomming_messages();
+    ~lora_incomming_messages();
+    bool addMessage(lora_packet packet);
+    uint32_t find(char * id);
+    std::vector <lora_packet> getMessages(char *id);
+};
