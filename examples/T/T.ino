cac03c2f62a61cf959ce92064529398809bcbf86
renemichelnunes <rene.michel@gmail.com>
2024-01-22 10:31:28 -0300

    DX mode

--- examples/T/T.ino
+++ examples/T/T.ino
@@ -542,7 +542,71 @@ void listen(){
     gotPacket = true;
 }
 
-void DXMode(lv_event_t * e)
+bool normalMode(){
+    Serial.println("normal mode");
+    if(xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE){
+        radio.sleep(false);
+        radio.reset();
+        radio.begin();
+        // set carrier frequency to 868.0 MHz
+        if (radio.setFrequency(RADIO_FREQ) == RADIOLIB_ERR_INVALID_FREQUENCY) {
+            Serial.println(F("Selected frequency is invalid for this module!"));
+            return false;
+        }
+
+        // set bandwidth to 250 kHz
+        if (radio.setBandwidth(250.0) == RADIOLIB_ERR_INVALID_BANDWIDTH) {
+            Serial.println(F("Selected bandwidth is invalid for this module!"));
+            return false;
+        }
+
+        // set spreading factor to 10
+        if (radio.setSpreadingFactor(10) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR) {
+            Serial.println(F("Selected spreading factor is invalid for this module!"));
+            return false;
+        }
+
+        // set coding rate to 6
+        if (radio.setCodingRate(6) == RADIOLIB_ERR_INVALID_CODING_RATE) {
+            Serial.println(F("Selected coding rate is invalid for this module!"));
+            return false;
+        }
+
+        // set LoRa sync word to 0xAB
+        if (radio.setSyncWord(0xAB) != RADIOLIB_ERR_NONE) {
+            Serial.println(F("Unable to set sync word!"));
+            return false;
+        }
+
+        // set output power to 10 dBm (accepted range is -17 - 22 dBm)
+        if (radio.setOutputPower(17) == RADIOLIB_ERR_INVALID_OUTPUT_POWER) {
+            Serial.println(F("Selected output power is invalid for this module!"));
+            return false;
+        }
+
+        // set over current protection limit to 140 mA (accepted range is 45 - 140 mA)
+        // NOTE: set value to 0 to disable overcurrent protection
+        if (radio.setCurrentLimit(140) == RADIOLIB_ERR_INVALID_CURRENT_LIMIT) {
+            Serial.println(F("Selected current limit is invalid for this module!"));
+            return false;
+        }
+
+        // set LoRa preamble length to 15 symbols (accepted range is 0 - 65535)
+        if (radio.setPreambleLength(15) == RADIOLIB_ERR_INVALID_PREAMBLE_LENGTH) {
+            Serial.println(F("Selected preamble length is invalid for this module!"));
+            return false;
+        }
+
+        // disable CRC
+        if (radio.setCRC(false) == RADIOLIB_ERR_INVALID_CRC_CONFIGURATION) {
+            Serial.println(F("Selected CRC is invalid for this module!"));
+            return false;
+        }
+        xSemaphoreGive(xSemaphore);
+    }
+}
+
+bool DXMode()
 {
     /*
     Distance - SF/BW
@@ -555,66 +619,69 @@ void DXMode(lv_event_t * e)
     100km - 11/250
     125km - 12/250
     */
-    digitalWrite(BOARD_SDCARD_CS, HIGH);
-    digitalWrite(RADIO_CS_PIN, HIGH);
-    digitalWrite(BOARD_TFT_CS, HIGH);
-    SPI.end();
-    SPI.begin(BOARD_SPI_SCK, BOARD_SPI_MISO, BOARD_SPI_MOSI); //SD
+    Serial.println("DX mode");
+    if(xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE){
+        radio.sleep(false);
+        radio.reset();
+        radio.begin();
 
-    // set carrier frequency to 868.0 MHz
-    if (radio.setFrequency(RADIO_FREQ) == RADIOLIB_ERR_INVALID_FREQUENCY) {
-        Serial.println(F("Selected frequency is invalid for this module!"));
-        //return false;
-    }
+        // set carrier frequency to 868.0 MHz
+        if (radio.setFrequency(RADIO_FREQ) == RADIOLIB_ERR_INVALID_FREQUENCY) {
+            Serial.println(F("Selected frequency is invalid for this module!"));
+            return false;
+        }
 
-    // set bandwidth to 250 kHz
-    if (radio.setBandwidth(125.0) == RADIOLIB_ERR_INVALID_BANDWIDTH) {
-        Serial.println(F("Selected bandwidth is invalid for this module!"));
-        //return false;
-    }
+        // set bandwidth to 250 kHz
+        if (radio.setBandwidth(125.0) == RADIOLIB_ERR_INVALID_BANDWIDTH) {
+            Serial.println(F("Selected bandwidth is invalid for this module!"));
+            return false;
+        }
 
-    // set spreading factor to 10
-    if (radio.setSpreadingFactor(12) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR) {
-        Serial.println(F("Selected spreading factor is invalid for this module!"));
-        //return false;
-    }
+        // set spreading factor to 10
+        if (radio.setSpreadingFactor(12) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR) {
+            Serial.println(F("Selected spreading factor is invalid for this module!"));
+            return false;
+        }
 
-    // set coding rate to 6
-    if (radio.setCodingRate(6) == RADIOLIB_ERR_INVALID_CODING_RATE) {
-        Serial.println(F("Selected coding rate is invalid for this module!"));
-        //return false;
-    }
+        // set coding rate to 6
+        if (radio.setCodingRate(6) == RADIOLIB_ERR_INVALID_CODING_RATE) {
+            Serial.println(F("Selected coding rate is invalid for this module!"));
+            return false;
+        }
 
-    // set LoRa sync word to 0xAB
-    if (radio.setSyncWord(0xAB) != RADIOLIB_ERR_NONE) {
-        Serial.println(F("Unable to set sync word!"));
-        //return false;
-    }
+        // set LoRa sync word to 0xAB
+        if (radio.setSyncWord(0xAB) != RADIOLIB_ERR_NONE) {
+            Serial.println(F("Unable to set sync word!"));
+            return false;
+        }
 
-    // set output power to 10 dBm (accepted range is -17 - 22 dBm)
-    if (radio.setOutputPower(22) == RADIOLIB_ERR_INVALID_OUTPUT_POWER) {
-        Serial.println(F("Selected output power is invalid for this module!"));
-        //return false;
-    }
+        // set output power to 10 dBm (accepted range is -17 - 22 dBm)
+        if (radio.setOutputPower(22) == RADIOLIB_ERR_INVALID_OUTPUT_POWER) {
+            Serial.println(F("Selected output power is invalid for this module!"));
+            return false;
+        }
 
-    // set over current protection limit to 140 mA (accepted range is 45 - 140 mA)
-    // NOTE: set value to 0 to disable overcurrent protection
-    if (radio.setCurrentLimit(140) == RADIOLIB_ERR_INVALID_CURRENT_LIMIT) {
-        Serial.println(F("Selected current limit is invalid for this module!"));
-        //return false;
-    }
+        // set over current protection limit to 140 mA (accepted range is 45 - 140 mA)
+        // NOTE: set value to 0 to disable overcurrent protection
+        if (radio.setCurrentLimit(140) == RADIOLIB_ERR_INVALID_CURRENT_LIMIT) {
+            Serial.println(F("Selected current limit is invalid for this module!"));
+            return false;
+        }
 
-    // set LoRa preamble length to 15 symbols (accepted range is 0 - 65535)
-    if (radio.setPreambleLength(15) == RADIOLIB_ERR_INVALID_PREAMBLE_LENGTH) {
-        Serial.println(F("Selected preamble length is invalid for this module!"));
-        //return false;
-    }
+        // set LoRa preamble length to 15 symbols (accepted range is 0 - 65535)
+        if (radio.setPreambleLength(15) == RADIOLIB_ERR_INVALID_PREAMBLE_LENGTH) {
+            Serial.println(F("Selected preamble length is invalid for this module!"));
+            return false;
+        }
 
-    // disable CRC
-    if (radio.setCRC(true) == RADIOLIB_ERR_INVALID_CRC_CONFIGURATION) {
-        Serial.println(F("Selected CRC is invalid for this module!"));
-        //return false;
+        // disable CRC
+        if (radio.setCRC(false) == RADIOLIB_ERR_INVALID_CRC_CONFIGURATION) {
+            Serial.println(F("Selected CRC is invalid for this module!"));
+            return false;
+        }
+        xSemaphoreGive(xSemaphore);
     }
+    return true;
     
 }
 
@@ -1033,8 +1100,16 @@ void generateID(lv_event_t * e){
 
 void DX(lv_event_t * e){
     if(lv_obj_has_state(frm_settings_switch_dx, LV_STATE_CHECKED)){
-        Serial.println("DX mode on");
+        if(DXMode()){
+            Serial.println("DX mode on");
+            notification_list.add(LV_SYMBOL_SETTINGS " DX mode");
+        }else{
+            notification_list.add(LV_SYMBOL_SETTINGS " DX mode failed");
+            lv_obj_clear_state(frm_settings_switch_dx, LV_STATE_CHECKED);
+        }
     }else{
+        normalMode();
+        notification_list.add(LV_SYMBOL_SETTINGS " Normal mode");
         Serial.println("DX mode off");
     }
 }
