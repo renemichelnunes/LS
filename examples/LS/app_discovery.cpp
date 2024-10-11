9c300f5c60ecf043a875f83cc2d4cad284e93d66
renemichelnunes <rene.michel@gmail.com>
2024-10-11 07:00:06 -0300

    app_discovery

--- examples/LS/app_discovery.cpp
+++ examples/LS/app_discovery.cpp
@@ -1,15 +1,17 @@
 #include <app_discovery.hpp>
 
-discovery_node::discovery_node(char *node_id, uint8_t hops)
+discovery_node::discovery_node(char * node_id, uint8_t hops, uint16_t gridX, uint16_t gridY)
 {
-    strcpy(this->node_id, node_id);
-    this->hops = hops;
+    strcpy(this->gridLocalization.node_id, node_id);
+    this->gridLocalization.hops = hops;
+    this->gridLocalization.gridX = gridX;
+    this->gridLocalization.gridY = gridY;
 }
 
 bool discovery_app::exists(const char *node_id)
 {
     for(discovery_node node : this->list){
-        if(strcmp(node.node_id, node_id) == 0){
+        if(strcmp(node.gridLocalization.node_id, node_id) == 0){
             return true;
         }
     }
@@ -19,7 +21,7 @@ bool discovery_app::exists(const char *node_id)
 bool discovery_app::add(discovery_node node)
 {
     for(discovery_node dn : this->list)
-        if(strcmp(dn.node_id, node.node_id) == 0)
+        if(strcmp(dn.gridLocalization.node_id, node.gridLocalization.node_id) == 0)
             return false;
     this->list.push_back(node);
     return true;
@@ -28,9 +30,17 @@ bool discovery_app::add(discovery_node node)
 discovery_node * discovery_app::getNode(const char *node_id)
 {
     for(uint8_t i = 0; i < this->list.size(); i++){
-        if(strcmp(this->list[i].node_id, node_id) == 0){
+        if(strcmp(this->list[i].gridLocalization.node_id, node_id) == 0){
             return &this->list[i];
         }
     }
     return nullptr;
+}
+
+uint32_t discovery_app::hopsTo(const char *node_id)
+{
+    for(discovery_node node : this->list)
+        if(strcmp(node.gridLocalization.node_id, node_id) == 0)
+            return node.gridLocalization.hops;
+    return 0;
 }
--- examples/LS/app_discovery.hpp
+++ examples/LS/app_discovery.hpp
@@ -4,23 +4,17 @@
 
 #define APP_DISCOVERY 1
 
-struct node_vicinity{
+struct grid_localization{
     char node_id[7] = {'\0'};
-    char nb1[7] = {'\0'};
-    char nb1[7] = {'\0'};
-    char nb1[7] = {'\0'};
-    char nb1[7] = {'\0'};
-    char nb1[7] = {'\0'};
-    char nb1[7] = {'\0'};
-    char nb1[7] = {'\0'};
-    char nb1[7] = {'\0'};
+    uint16_t gridX = 0;
+    uint16_t gridY = 0;
+    uint16_t hops = 0;
 };
 
 class discovery_node{
     public:
-        char node_id[7];
-        uint8_t hops;
-        discovery_node(char * node_id, uint8_t hops);
+        grid_localization gridLocalization;
+        discovery_node(char * node_id, uint8_t hops, uint16_t gridX, uint16_t gridY);
 };
 
 class discovery_app{
@@ -30,6 +24,5 @@ class discovery_app{
         bool exists(const char * node_id);
         bool add(discovery_node node);
         discovery_node * getNode(const char * node_id);
-        uint32_t estimatedDeliveryTime(const char * node_id);
-        uint32_t estimatedResponseTime(const char * node_id);
+        uint32_t hopsTo(const char * node_id);
 };
