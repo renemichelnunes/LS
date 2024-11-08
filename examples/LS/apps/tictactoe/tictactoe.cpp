8164d6689d1dc1ebecd81f351e64d8c79a96a1f4
renemichelnunes <rene.michel@gmail.com>
2024-11-08 12:47:00 -0300

    fix

--- examples/LS/apps/tictactoe/tictactoe.cpp
+++ examples/LS/apps/tictactoe/tictactoe.cpp
@@ -1,43 +1,110 @@
 #include <apps/tictactoe/tictactoe.hpp>
-#include "tictactoe.hpp"
+
+#define user_id "aaaaaa"
+#define destiny_player "bbbbbb"
+
+TaskHandle_t cpu_task = NULL;
 
 static void hide(lv_event_t *e)
 {
     lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * frm = (lv_obj_t*)lv_event_get_user_data(e);
+    tictactoe * t = (tictactoe*)lv_event_get_user_data(e);
     if(code == LV_EVENT_SHORT_CLICKED){
-        if(frm){
-            lv_obj_add_flag(frm, LV_OBJ_FLAG_HIDDEN);
-            
+        if(t->frm_main){
+            lv_obj_add_flag(t->frm_main, LV_OBJ_FLAG_HIDDEN);
+            if(cpu_task){
+                vTaskDelete(cpu_task);
+                cpu_task = NULL;
+                Serial.println("cpu_task deleted");
+            }
         }
     }
 }
 
-static void btn_event_handler(lv_event_t* e) {
-    lv_obj_t* btn = lv_event_get_target(e);
+void new_game(lv_event_t * e){
     lv_event_code_t code = lv_event_get_code(e);
-    tictactoe * tic = (tictactoe*)lv_event_get_user_data(e);
-    int row = lv_obj_get_y(btn) / 15;    // A posição do botão no eixo Y (linha)
-    int col = lv_obj_get_x(btn) / 15;   // A posição do botão no eixo X (coluna)
+    tictactoe * ttt = (tictactoe*)lv_event_get_user_data(e);
 
-    // Se o botão ainda estiver vazio e o jogo estiver ativo
-    if (tic->board[row][col] == ' ' && tic->active) {
-        // Atualiza o estado do botão
-        tic->board[row][col] = tic->player;
-        
-        // Definir o texto no botão (X ou O)
-        lv_label_set_text(lv_obj_get_child(btn, 0), &tic->player);
+    if(code == LV_EVENT_CLICKED){
+        ttt->active = true;
+        ttt->cpu_turn = false;
+        ttt->init_board();
+    }
+}
 
-        // Verificar se o jogador atual venceu
-        if (tic->checkVictory()) {
-            lv_label_set_text(tic->frm_tictactoe_btn_title_lbl, "You won!");
-            tic->active = false;
-        } else if (tic->checkDraw()) {
-            lv_label_set_text(tic->frm_tictactoe_btn_title_lbl, "Draw!");
-            tic->active = false;
-        } else {
-            // Trocar jogador
-            tic->player = (tic->player == 'X') ? 'O' : 'X';
+void btn_event_handler(lv_event_t* e) {
+    lv_obj_t* btn = (lv_obj_t*)lv_event_get_target(e);
+    tictactoe * ttt = (tictactoe*)lv_event_get_user_data(e);
+    char msg[30] = {'\0'};
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED){
+        int linha = lv_obj_get_y(btn) / 52;    // A posição do botão no eixo Y (linha)
+        int coluna = lv_obj_get_x(btn) / 52;   // A posição do botão no eixo X (coluna)
+
+        ttt_mov mov;
+        mov.row = linha;
+        mov.col = coluna;
+
+        // Se o botão ainda estiver vazio e o jogo estiver ativoSem vencedores
+        if (ttt->board[linha][coluna] == ' ' && ttt->active) {
+            // Atualiza o estado do botão
+            ttt->board[linha][coluna] = ttt->player;
+            char p[2] = {'\0'};
+            p[0] = ttt->player;
+            // Definir o texto no botão (X ou O)
+            lv_label_set_text(lv_obj_get_child(btn, 0), p);
+            // Send the move
+            if(ttt->player == 'X'){
+                lora_packet lp;
+                strcpy(lp.id, generate_ID(6).c_str());
+                lp.app_id = APP_TICTACTOE;
+                strcpy(lp.sender, user_id);
+                strcpy(lp.destiny, destiny_player);
+                lp.data_size = sizeof(mov);
+                memcpy(lp.data, &mov, lp.data_size);
+                lp.crc = calculate_data_crc(&mov, lp.data_size);
+                printf("ID %s\nAPP_ID %d\nSender %s\nDestiny %s\nData size %d\n", lp.id, lp.app_id, lp.sender, lp.destiny, lp.data_size);
+                uint8_t c = 0;
+                for(uint8_t i = 0; i < sizeof(lp.data); i++){
+                    printf("%x ", lp.data[i]);
+                    c++;
+                    if(c == 20){
+                        printf("\n");
+                        c = 0;
+                    }
+                }
+                printf("\n");
+            }
+
+            //lv_label_set_text(frame_game_lbl, "waiting the player...");
+            // transmit_pck_list.add(lp);
+
+            // Verificar se o jogador atual venceu
+            if (ttt->checkVictory()) {
+                if(ttt->player == 'X')
+                    lv_label_set_text(ttt->frame_game_lbl, "You won!");
+                else
+                    lv_label_set_text(ttt->frame_game_lbl, "Better luck next time.");
+                ttt->active = false;
+                lv_obj_clear_flag(ttt->frame_game_new_btn, LV_OBJ_FLAG_HIDDEN);
+            } else if (ttt->checkDraw()) {
+                lv_label_set_text(ttt->frame_game_lbl, "Draw!");
+                ttt->active = false;
+                lv_obj_clear_flag(ttt->frame_game_new_btn, LV_OBJ_FLAG_HIDDEN);
+            } else {
+                // Change player
+                ttt->player = (ttt->player == 'X') ? 'O' : 'X';
+                if(ttt->player == 'X'){
+                    strcpy(msg, "Your turn.");
+
+                }
+                else{
+                    strcpy(msg, "Friend's turn.");
+                    ttt->cpu_turn = true;
+                }
+                lv_label_set_text(ttt->frame_game_lbl, msg);
+            }
         }
     }
 }
@@ -47,47 +114,44 @@ void tictactoe::initUI(lv_obj_t * parent)
     // Main form
     this->parent = parent;
     if(this->parent){
-        this->frm_tictactoe = lv_obj_create(this->parent);
-        lv_obj_set_size(this->frm_tictactoe, LV_HOR_RES, LV_VER_RES);
-        lv_obj_clear_flag(this->frm_tictactoe, LV_OBJ_FLAG_SCROLLABLE);
-            lv_obj_add_flag(this->frm_tictactoe, LV_OBJ_FLAG_HIDDEN);
-        // Title button
-        this->frm_tictactoe_btn_title = lv_btn_create(this->frm_tictactoe);
-        lv_obj_set_size(this->frm_tictactoe_btn_title, 200, 20);
-        lv_obj_align(this->frm_tictactoe_btn_title, LV_ALIGN_TOP_LEFT, -15, -15);
+         // Main form
+        this->frm_main = lv_obj_create(this->parent);
+        lv_obj_set_size(this->frm_main, 320, 240);
+        //lv_obj_set_style_bg_color(frm_main, lv_color_hex(0xffffff), LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(this->frm_main, 255, LV_PART_MAIN);
+        lv_obj_align(this->frm_main, LV_ALIGN_TOP_MID, 0, 0);
+        lv_obj_set_style_radius(this->frm_main, 0, LV_PART_MAIN);
+        lv_obj_set_style_border_width(this->frm_main, 0, LV_PART_MAIN);
+        lv_obj_clear_flag(this->frm_main, LV_OBJ_FLAG_SCROLLABLE);
 
-        this->frm_tictactoe_btn_title_lbl = lv_label_create(this->frm_tictactoe_btn_title);
-        lv_label_set_text(this->frm_tictactoe_btn_title_lbl, "TIC TAC TOE");
-        lv_obj_set_align(this->frm_tictactoe_btn_title_lbl, LV_ALIGN_LEFT_MID);
+        this->frm_main_title_btn = lv_btn_create(this->frm_main);
+        lv_obj_set_size(this->frm_main_title_btn, 310, 20);
+        lv_obj_align(this->frm_main_title_btn, LV_ALIGN_TOP_LEFT, -10, -10);
 
-        // Close button
-        this->frm_tictactoe_btn_back = lv_btn_create(this->frm_tictactoe);
-        lv_obj_set_size(this->frm_tictactoe_btn_back, 50, 20);
-        lv_obj_align(this->frm_tictactoe_btn_back, LV_ALIGN_TOP_RIGHT, 15, -15);
-        lv_obj_add_event_cb(this->frm_tictactoe_btn_back, hide, LV_EVENT_SHORT_CLICKED, this->frm_tictactoe);
+        this->frm_main_title_btn_lbl = lv_label_create(this->frm_main_title_btn);
+        lv_label_set_text(this->frm_main_title_btn_lbl, "TIC-TAC-TOE");
+        lv_obj_set_align(this->frm_main_title_btn_lbl, LV_ALIGN_LEFT_MID);
+
+        this->frm_main_back_btn = lv_btn_create(this->frm_main);
+        lv_obj_set_size(this->frm_main_back_btn, 50, 20);
+        lv_obj_align(this->frm_main_back_btn, LV_ALIGN_TOP_RIGHT, 10, -10);
+        lv_obj_add_event_cb(this->frm_main_back_btn, hide, LV_EVENT_SHORT_CLICKED, this);
+
+        this->frm_main_back_btn_lbl = lv_label_create(this->frm_main_back_btn);
+        lv_label_set_text(this->frm_main_back_btn_lbl, "Back");
+        lv_obj_set_align(this->frm_main_back_btn_lbl, LV_ALIGN_CENTER);
+
+        // Frame dos nós
+        this->frame_game = lv_obj_create(this->frm_main);
+        lv_obj_set_size(this->frame_game, 320, 210);
+        lv_obj_set_style_bg_color(this->frame_game, lv_color_hex(0xeeeeee), LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(this->frame_game, 255, LV_PART_MAIN);
+        lv_obj_align(this->frame_game, LV_ALIGN_TOP_MID, 0, 15);
+        lv_obj_set_style_radius(this->frame_game, 0, LV_PART_MAIN);
+        lv_obj_set_style_border_width(this->frame_game, 0, LV_PART_MAIN);
+        lv_obj_set_scroll_dir(this->frame_game, LV_DIR_VER);
         
-        this->frm_tictactoe_btn_back_lbl = lv_label_create(this->frm_tictactoe_btn_back);
-        lv_label_set_text(this->frm_tictactoe_btn_back_lbl, "Back");
-        lv_obj_set_align(this->frm_tictactoe_btn_back_lbl, LV_ALIGN_CENTER);
-
-        // Set the board
-        this->frm_tictactoe_board = lv_obj_create(this->frm_tictactoe);
-        lv_obj_set_size(this->frm_tictactoe_board, 200, 200);
-        lv_obj_align(this->frm_tictactoe_board, LV_ALIGN_TOP_LEFT, -15, -15);
-        lv_obj_set_style_bg_color(this->frm_tictactoe_board, lv_color_hex(0xffaa00), LV_PART_MAIN);
-
-        // Put the buttons
-        for(uint8_t i = 0; i < 3; i++){
-            for(uint8_t j = 0; j < 3; j++){
-                this->btns[i][j] = lv_btn_create(this->frm_tictactoe_board);
-                lv_obj_set_size(this->btns[i][j], 15, 15);
-                lv_obj_set_pos(this->btns[i][j], j * 15, i * 15); // Definir a posição do botão
-                lv_obj_add_event_cb(this->btns[i][j], btn_event_handler, LV_EVENT_CLICKED, this);
-                lv_obj_t* label = lv_label_create(this->btns[i][j]);
-                lv_label_set_text(label, "");
-            }
-        }
-
+        this->init_board();
         Serial.println("tictactoe_app::initUI() - READY");
     }else
         Serial.println("tictactoe_app::initUI() - parent NULL");
@@ -122,4 +186,88 @@ bool tictactoe::checkDraw()
         }
     }
     return true;
+}
+
+void tictactoe::simulate_click(uint8_t row, uint8_t col)
+{
+    lv_obj_t * btn;
+
+    if(this->board[row][col] == ' '){
+        btn = this->btns[row][col];
+        lv_event_send(btn, LV_EVENT_CLICKED, NULL);
+        this->cpu_turn = false;
+    }
+}
+
+void tictactoe::init_board()
+{
+    this->player = 'X';
+    this->cpu_turn = false;
+    this->active = true;
+    lv_obj_clean(this->frame_game);
+    // Inicializar o tabuleiro com 9 botões
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            this->btns[i][j] = lv_btn_create(this->frame_game);
+            lv_obj_set_size(this->btns[i][j], 50, 50);
+            lv_obj_set_pos(this->btns[i][j], j * 52, i * 52); // Definir a posição do botão
+            lv_obj_add_event_cb(this->btns[i][j], btn_event_handler, LV_EVENT_CLICKED, this);
+            lv_obj_t* label = lv_label_create(this->btns[i][j]);
+            lv_label_set_text(label, ""); // Inicialmente, os botões estão vazios
+        }
+    }
+
+    // Inicializar o tabuleiro
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            this->board[i][j] = ' ';
+        }
+    }
+
+    this->frame_game_lbl = lv_label_create(this->frame_game);
+    lv_obj_align(this->frame_game_lbl, LV_ALIGN_BOTTOM_LEFT, 0,0);
+    lv_label_set_text(this->frame_game_lbl, "Let's play!");
+
+    this->frame_game_new_btn = lv_btn_create(this->frame_game);
+    lv_obj_set_size(this->frame_game_new_btn, 40, 20);
+    lv_obj_align(this->frame_game_new_btn, LV_ALIGN_TOP_RIGHT, 0, 0);
+    lv_obj_add_event_cb(this->frame_game_new_btn, new_game, LV_EVENT_CLICKED, this);
+
+    this->frame_game_new_btn_lbl = lv_label_create(this->frame_game_new_btn);
+    lv_label_set_text(this->frame_game_new_btn_lbl, "New");
+    lv_obj_set_align(this->frame_game_new_btn_lbl, LV_ALIGN_CENTER);
+    lv_obj_add_flag(this->frame_game_new_btn, LV_OBJ_FLAG_HIDDEN);
+}
+
+static void cpu_move(void * ttt)
+{
+    tictactoe * t = (tictactoe*)ttt;
+    while(true){
+        if(t->cpu_turn){
+            uint8_t row = rand() % 3;
+            uint8_t col = rand() % 3;
+            t->simulate_click(row, col);
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+tictactoe::tictactoe()
+{
+    for(uint8_t i = 0; i < 3; i++)
+        for(uint8_t j = 0; j < 3; j++)
+            this->board[i][j] = ' ';
+
+    for(uint8_t i = 0; i < 3; i++){
+        for(uint8_t j = 0; j < 3; j++){
+            printf("%c ", this->board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    xTaskCreatePinnedToCore(cpu_move, "cpu_task", 6000, this, 1, &cpu_task, 1);
+    if(cpu_task){
+        Serial.println("cpu_task launched");
+    }
 }
--- examples/LS/apps/tictactoe/tictactoe.hpp
+++ examples/LS/apps/tictactoe/tictactoe.hpp
@@ -1,29 +1,47 @@
 #pragma once
 #include <Arduino.h>
 #include <lvgl.h>
+#include "lora_messages.hpp"
 
 #define APP_TICTACTOE 4
+
+struct ttt_mov{
+    uint8_t row = 0;
+    uint8_t col = 0;
+};
 
 class tictactoe{
     private:
         
     public:
     // UI objects
-    lv_obj_t * parent;
-    lv_obj_t * frm_tictactoe;
-    lv_obj_t * frm_tictactoe_btn_title;
-    lv_obj_t * frm_tictactoe_btn_title_lbl;
-    lv_obj_t * frm_tictactoe_btn_back;
-    lv_obj_t * frm_tictactoe_btn_back_lbl;
-    lv_obj_t * frm_tictactoe_board;
+    lv_obj_t * frm_main;
+    lv_obj_t * frm_main_title_btn;
+    lv_obj_t * frm_main_title_btn_lbl;
+    lv_obj_t * frm_main_back_btn;
+    lv_obj_t * frm_main_back_btn_lbl;
+    lv_obj_t * frame_game;
+    lv_obj_t * frame_game_lbl;
+    lv_obj_t * frame_game_new_btn;
+    lv_obj_t * frame_game_new_btn_lbl;
+    lv_obj_t * frm_tictactoe_players;
+    lv_obj_t * frm_tictactoe_players_btn_title;
+    lv_obj_t * frm_tictactoe_players_btn_title_lbl;
+    lv_obj_t * frm_tictactoe_players_btn_back;
+    lv_obj_t * frm_tictactoe_players_btn_back_lbl;
     lv_obj_t * btns[3][3];
-    
-    bool checkVictory();
-    bool checkDraw();
+    lv_obj_t * parent;
+
     char board[3][3];
     char player = 'X';
     bool active = true;
+    bool cpu_turn = false;
+
+    bool checkVictory();
+    bool checkDraw();
     void initUI(lv_obj_t * parent);
+    void init_board();
+    void simulate_click(uint8_t row, uint8_t col);
     tictactoe();
-    ~tictactoe();
+    //~tictactoe();
 };
